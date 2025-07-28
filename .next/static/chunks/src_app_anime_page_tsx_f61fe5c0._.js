b3c51ef3b2abead03ade819d7c46259978731185
bytes <daciabyte@gmail.com>
2025-07-28 16:09:24 +0200

    Add .next/static/chunks/src_app_anime_page_tsx_f61fe5c0._.js 1753711761321

---
+++ .next/static/chunks/src_app_anime_page_tsx_f61fe5c0._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_593636ea._.js",
+  "static/chunks/node_modules_fde2244f._.js"
+],
+    source: "dynamic"
+});
